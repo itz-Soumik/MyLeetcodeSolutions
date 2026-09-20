5f1ba1d16456c6eed466b6b7303308951a94a6e9
Soumik Saha <s.sahamma17@gmail.com>
2026-09-20 16:42:51 +0530

    Time: 3 ms (18.46%), Space: 85.1 MB (78.36%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -1,9 +1,11 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
         boolean isIncreasing = true, isDecreasing = true;
-        for (int i=0;i<nums.length - 1;i++) {
+        int i = 0;
+        while(i<nums.length - 1) {
             if(nums[i] > nums[i+1]) isIncreasing = false;
             else if(nums[i] < nums[i+1]) isDecreasing = false;
+            i++;
         }
         
         if (isIncreasing || isDecreasing) return true;
